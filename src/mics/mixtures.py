6cc4a2071d6d73a09096e93215eb4f628299079c
Charlles Abreu <abreu@eq.ufrj.br>
2017-10-14 19:02:25 -0300

    Implementation of Newton-Raphson iterations

--- src/mics/mixtures.py
+++ src/mics/mixtures.py
@@ -35,6 +35,7 @@ class Mixture:
         info(verbose, "Setting up MICS case:", title)
 
         m = self.m = len(states)
+        S = range(m)
         info(verbose, "Number of states:", m)
 
         if m == 0:
@@ -45,20 +46,63 @@ class Mixture:
         properties = names(0)
         info(verbose, "Properties:", ", ".join(properties))
 
-        if any([names(i) != properties for i in range(m)]):
+        if any([names(i) != properties for i in S]):
             raise ValueError("inconsistent data")
 
-        n = self.n = [states[i].sample.shape[0] for i in range(m)]
+        n = self.n = [states[i].sample.shape[0] for i in S]
         info(verbose, "Sample sizes:", n)
 
-        neff = np.array([states[i].neff for i in range(m)])
+        neff = np.array([states[i].neff for i in S])
         info(verbose, "Effective sample sizes:", neff)
 
         pi = self.pi = neff/sum(neff)
         info(verbose, "Mixture composition:", pi)
 
-        potentials = [states[i].potential for i in range(m)]
-        u = [multimap(potentials, states[i].sample) for i in range(m)]
+        potentials = [states[i].potential for i in S]
+        u = [multimap(potentials, states[i].sample) for i in S]
 
         f = self.f = overlapSampling(u)
         info(verbose, "Initial free-energy guess:", f)
+
+        P = self.P = [np.empty([m, n[i]]) for i in S]
+        self.u0 = [np.empty([1, n[i]]) for i in S]
+
+        def iteration():
+            self._compute(f, u)
+            pm = [np.mean(P[i], axis=1) for i in S]
+            p0 = sum(pi[i]*pm[i] for i in S)
+            B0 = np.diag(p0) - sum(P[i].dot(P[i].T)*pi[i]/n[i] for i in S)  # Optimize here
+            df = np.linalg.solve(B0[1:m, 1:m], (pi - p0)[1:m])
+            return df, p0, B0
+
+        iter = 0
+        df, p0, B0 = iteration()
+        while any(abs(df) > tol):
+            iter += 1
+            f[1:m] += df
+            df, p0, B0 = iteration()
+        info(verbose, "Free energies after %d iterations:" % iter, f)
+
+    def _compute(self, f, u):
+        """
+        Computes probabilities
+        """
+        m = len(u)
+        g = (f + np.log(self.pi)).reshape([m, 1])
+        for i in range(m):
+            x = g - u[i]
+            xmax = np.amax(x, axis=0)
+            numer = np.exp(x - xmax)
+            denom = np.sum(numer, axis=0)
+            self.P[i] = numer / denom
+            self.u0[i] = -(xmax + np.log(denom))
+
+#     def means(self, X):
+#         """
+#         Computes the means of a set of properties
+#         """
+#         m = len(X)
+#         n = X[0].shape[0]
+#         xm = [np.empty(m)
+#         for i in range(len(X)):
+#             x
