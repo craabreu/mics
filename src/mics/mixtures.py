d31bbce71558b2716cd196321660fcde3b0ff17d
Charlles Abreu <abreu@eq.ufrj.br>
2017-10-17 14:07:58 -0200

    Initial implementation of property reweighting

--- src/mics/mixtures.py
+++ src/mics/mixtures.py
@@ -13,6 +13,7 @@ import numpy as np
 import pandas as pd
 
 from mics.utils import covariance
+from mics.utils import cross_covariance
 from mics.utils import info
 from mics.utils import multimap
 from mics.utils import overlapSampling
@@ -34,11 +35,13 @@ class mixture:
 
     """
 
+    # ======================================================================================
     def __init__(self, states, title="Untitled", verbose=False, tol=1.0E-8):
 
         info(verbose, "Setting up MICS case:", title)
 
         m = self.m = len(states)
+        self.states = states
         S = range(m)
         info(verbose, "Number of states:", m)
 
@@ -69,22 +72,23 @@ class mixture:
         info(verbose, "Initial free-energy guess:", f)
 
         self.P = [np.empty([m, n[i]]) for i in S]
+        self.pm = [np.empty(m) for i in S]
         self.u0 = [np.empty([1, n[i]]) for i in S]
 
         iter = 1
-        df, pm, p0, B0 = self._newton_raphson_iteration(u)
+        df, B0 = self._newton_raphson_iteration(u)
         while any(abs(df) > tol):
             iter += 1
             f[1:m] += df
-            df, pm, p0, B0 = self._newton_raphson_iteration(u)
+            df, B0 = self._newton_raphson_iteration(u)
         info(verbose, "Free energies after %d iterations:" % iter, f)
 
         iB0 = self.iB0 = pinv(B0)
-        pm = [np.mean(self.P[i], axis=1) for i in S]
-        S0 = sum(pi[i]**2*covariance(self.P[i], pm[i], states[i].b) for i in S)
+        S0 = sum(pi[i]**2*covariance(self.P[i], self.pm[i], states[i].b) for i in S)
         self.Theta = iB0.dot(S0.dot(iB0))
         info(verbose, "Free-energy covariance matrix:", self.Theta)
 
+    # ======================================================================================
     def free_energies(self):
         """
         Returns a data frame containing the relative free energies of the sampled states
@@ -95,22 +99,63 @@ class mixture:
         df = np.sqrt([T[i, i] - 2*T[i, 0] + T[0, 0] for i in range(self.m)])
         return pd.DataFrame(data={'f': self.f, 'δf': df})
 
+    # ======================================================================================
+    def reweight(self, properties, potential, parameter):
+        """
+        Performs reweighting of the properties computed by `functions` from the mixture to
+        the states determined by the provided `potential` with all `parameter` values.
+
+        """
+        state = self.states
+        m = self.m
+        pi = self.pi
+        P = self.P
+        pm = self.pm
+        S = range(m)
+        b = [state[i].b for i in S]
+
+        def compute(x):
+            return np.vstack([np.ones(x.shape[0]), multimap(properties, x)])
+
+        z = [compute(state[i].sample) for i in S]
+
+        y0 = []
+        Xi = []
+        for value in parameter:
+            u = [multimap([lambda x: potential(x, value)], state[i].sample) for i in S]
+            du = [self.u0[i] - u[i] for i in S]
+            maxdu = np.amax([np.amax(du[i]) for i in S])
+            y = [np.exp(du[i] - maxdu)*z[i] for i in S]
+            ym = [np.mean(y[i], axis=1) for i in S]
+            Syy = [covariance(y[i], ym[i], b[i]) for i in S]
+            Spy = [cross_covariance(P[i], pm[i], y[i], ym[i], b[i]) for i in S]
+            Sy0y0 = sum(pi[i]**2*Syy[i] for i in S)
+            Sp0y0 = sum(pi[i]**2*Spy[i] for i in S)
+            Z0 = -sum(P[i].dot(y[i].T)*pi[i]/state[i].n for i in S)
+            A = Z0.T.dot(self.iB0.dot(Sp0y0))
+            y0.append(sum(pi[i]*ym[i] for i in S))
+            Xi.append(Sy0y0 + A + A.T + Z0.T.dot(self.Theta.dot(Z0)))
+
+        for k in range(len(parameter)):
+            print(y0[k][1]/y0[k][0], y0[k][2]/y0[k][0])
+
+    # ======================================================================================
     def _newton_raphson_iteration(self, u):
         m = self.m
         P = self.P
         pi = self.pi
         S = range(m)
         g = (self.f + np.log(self.pi)).reshape([m, 1])
-        for i in range(m):
+        for i in S:
             x = g - u[i]
             xmax = np.amax(x, axis=0)
             numer = np.exp(x - xmax)
             denom = np.sum(numer, axis=0)
             self.P[i] = numer / denom
             self.u0[i] = -(xmax + np.log(denom))
+            self.pm[i] = np.mean(P[i], axis=1)
 
-        pm = [np.mean(P[i], axis=1) for i in S]
-        p0 = sum(pi[i]*pm[i] for i in S)
+        p0 = sum(pi[i]*self.pm[i] for i in S)
         B0 = np.diag(p0) - sum(P[i].dot(P[i].T)*pi[i]/self.n[i] for i in S)  # Optimize here
         df = np.linalg.solve(B0[1:m, 1:m], (pi - p0)[1:m])
-        return df, pm, p0, B0
+        return df, B0
--- tests/test_mics.py
+++ tests/test_mics.py
@@ -16,43 +16,20 @@ beta = 1.6773985789
 data = ["tests/data/log_%d.dat" % (i + 1) for i in range(m)]
 
 
-def solution_1():  # Closures
-
-    def pot_gen(j):
-        return lambda x: beta*x['E'+str(j+1)]
-
-    def diff_gen(j):
-        return lambda x: pot_gen(min(j+1, m-1))(x) - pot_gen(max(j-1, 0))(x)
-
-    states = []
-    for i in range(m):
-        sample = pd.read_csv(data[i], sep=' ')
-        potential = pot_gen(i)
-        difference = diff_gen(i)
-        states.append(mics.state(sample, potential, difference))
-
-    return states
+def pot_fun(x, beta, j):
+    return beta*x['E'+str(j+1)]
 
 
-def solution_2():  # Partial evatualion:
-
-    def pot_fun(x, j):
-        return beta*x['E'+str(j+1)]
-
-    def diff_fun(x, j):
-        return pot_fun(x, min(j+1, m-1)) - pot_fun(x, max(j-1, 0))
-
-    states = []
-    for i in range(m):
-        sample = pd.read_csv(data[i], sep=' ')
-        potential = partial(pot_fun, j=i)
-        difference = partial(diff_fun, j=i)
-        states.append(mics.state(sample, potential, difference))
-
-    return states
+def diff_fun(x, beta, j):
+    return pot_fun(x, beta, min(j+1, m-1)) - pot_fun(x, beta, max(j-1, 0))
 
 
-states = solution_2()
+states = []
+for i in range(m):
+    sample = pd.read_csv(data[i], sep=' ')
+    potential = partial(pot_fun, beta=beta, j=i)
+    difference = partial(diff_fun, beta=beta, j=i)
+    states.append(mics.state(sample, potential, difference))
 
 neff = [101, 76, 69, 54]
 for i in range(4):
@@ -64,3 +41,8 @@ fe = mixture.free_energies()
 
 np.testing.assert_almost_equal(fe['f'][m-1], 3.62444247539)
 np.testing.assert_almost_equal(fe['δf'][m-1], 0.162805619863)
+
+properties = [lambda x: x.Press, lambda x: x.PotEng]
+potential = partial(pot_fun, j=m-1)
+
+props = mixture.reweight(properties, potential, parameter=[0.9*beta, beta, 1.1*beta])
